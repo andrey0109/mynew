e9ea019e7f306cc0bbc7bd69580afa958df58a8c
andrey0109 <andreyharutyunyan08@gmail.com>
2023-08-16 19:40:41 +0200

    myproject

---
+++ menu.js
@@ -0,0 +1,7 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+    const mainElement = document.querySelector("main");
+    mainElement.addEventListener("click", function() {
+        mainElement.style.backgroundColor = "lightblue";
+    });
+});
